efb7498370708b542d2d3d5b1eecff25be00b830
Alex Wilson <fonzmp@gmail.com>
2018-06-21 21:29:31 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,10 +46,7 @@ class UsersController < ApplicationController
   end
 
   get '/users/:slug' do
-    binding.pry
-    @user = current_user
-
-    @tweets = @user.tweets
+    
 
     erb :"users/show"
   end
