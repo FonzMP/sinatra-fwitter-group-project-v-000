55a60a8c67f699c489cb0a5f89e6f058a6b3ce9f
Alex Wilson <fonzmp@gmail.com>
2018-06-21 21:40:02 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,7 +9,7 @@ class TweetsController < ApplicationController
   end
 
   post '/tweets' do
-    @tweet = Tweet.create(content: params[:tweet][:content], user_id: current_user.id.to_i)
+    @tweet = Tweet.create(content: params[:tweet][:content], user_id: current_user.id)
 
     redirect '/tweets'
   end
