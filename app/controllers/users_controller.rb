bef13eb977b0fc9a32673f989cecca2b600bb7de
Alex Wilson <fonzmp@gmail.com>
2018-06-21 18:27:03 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
     @user = User.find_by(username: params[:username], email: params[:email], password: params[:password])
     if @user
       session[:user_id] = @user.id
+      redirect 'users/show'
   end
 
 
