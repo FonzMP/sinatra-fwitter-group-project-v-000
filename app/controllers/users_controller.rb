02396ac888bc545899ac4edd005aadc5c9482254
Alex Wilson <fonzmp@gmail.com>
2018-06-21 18:44:57 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username], email: params[:email], password: params[:password])
+    current_use
     if @user
       session[:user_id] = @user.id
       redirect '/tweets'
