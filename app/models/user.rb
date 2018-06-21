1906856af7e47a5dc99bdacbc9e66c8a21537f0c
Alex Wilson <fonzmp@gmail.com>
2018-06-21 21:21:45 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
     self.all.each do |user|
       @slug = user.slug
 
-      if user.slug = slug_name
+      if @slug = slug_name
         return user
       end
     end
