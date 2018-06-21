d095dc60e9a92c50ac10345e934dac658fdd92ea
Alex Wilson <fonzmp@gmail.com>
2018-06-21 19:12:57 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
     def find_user(params)
       @user = User.find_by(username: params[:username])
       if @user && @user.authenticate(params[:password])
-        session[:user_id]
+        session[:user_id] = @user.id
     end
 
   end
