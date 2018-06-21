40f373bc8adeb4274494ca55a5a547a55ce48a04
Alex Wilson <fonzmp@gmail.com>
2018-06-21 19:26:25 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,23 +45,7 @@ class UsersController < ApplicationController
     end
   end
 
-  helpers do
 
-    def current_user
-      if session.include?(:user_id)
-        @user ||= User.find(session[:user_id])
-      end
-    end
-
-    def create_user(params)
-      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-    end
-
-    def find_user(params)
-      @user = User.find_by(username: params[:username])
-    end
-
-  end
 
 
 end
